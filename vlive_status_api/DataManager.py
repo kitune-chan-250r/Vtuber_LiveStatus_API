48b1c858f9576e1a89a5048c0f907aec223a5cb9
kitune-chan-250r <41228665+kitune-chan-250r@users.noreply.github.com>
2019-09-28 22:40:47 +0900

    update DataManager.py
     DataManager.pyの処理をマルチスレッド化して若干高速化

--- vlive_status_api/DataManager.py
+++ vlive_status_api/DataManager.py
@@ -1,16 +1,12 @@
 import Vtuber_LiveStatus_API_lib as vlsa
 import requests, bs4, re, sys, os
-import django
-sys.path.append("api")
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vlive_status_api.settings')
-django.setup()
-from api.models import *
 from tqdm import tqdm
+import threading
+import time
 
 #BASE_URL = 'http://localhost:8000/api/' #local
 BASE_URL = 'https://vtuber-livestatus-api.herokuapp.com/api/' #heroku
 
-#channel idを受け取って放送中の場合は配信場所のURL、そうでない場合はFalseを返す
 def live_status(channelid):
     youtube_url = "https://www.youtube.com"
     channel_site_url = youtube_url + "/channel/"
@@ -34,7 +30,6 @@ def get_live_title(live_url):
     parsed = bs4.BeautifulSoup(html_data.content, "html.parser")
     return parsed.find('span', class_='watch-title metadata-updateable-title').text
 
-
 all_liver = vlsa.get(BASE_URL)
 on_liver = vlsa.get(BASE_URL + 'onlive/')
 
@@ -44,19 +39,32 @@ if len(on_liver) != 0:
 else:
     on_livers = []
 
-print(on_livers)
+div = len(all_liver) // 2
 
-for uid in uids:
-    status = live_status(uid)
-    print(status, uid)
-    if status is not False and uid not in on_livers:
-        try:
-            title = get_live_title(status)
-        except AttributeError:
-            title = "取得失敗"
-        data = {'uid': uid, 'live_title': title, 'live_url': status}
-        #on_liveに追加
-        res = vlsa.post(BASE_URL+'onlive', data)
-    elif status is False and uid in on_livers:
-        #on_liveから外す
-        res = vlsa.delete(BASE_URL+'onlive', uid)
+ll1 = uids[:div]
+ll2 = uids[div:]
+
+def reqtask(uids):
+    start = time.time()
+    for uid in uids:
+        print(uid, status)
+        status = live_status(uid)
+        #print(status, uid)
+        if status is not False and uid not in on_livers:
+            try:
+                title = get_live_title(status)
+            except AttributeError:
+                title = "取得失敗"
+            data = {'uid': uid, 'live_title': title, 'live_url': status}
+            #on_liveに追加
+            res = vlsa.post(BASE_URL+'onlive', data)
+        elif status is False and uid in on_livers:
+            #on_liveから外す
+            res = vlsa.delete(BASE_URL+'onlive', uid)
+
+th1 = threading.Thread(target=reqtask, args=(ll1,))
+th2 = threading.Thread(target=reqtask, args=(ll2,))
+
+th1.start()
+th2.start()
+
--- vlive_status_api/prayground.py
+++ vlive_status_api/prayground.py
@@ -1,5 +1,5 @@
 import Vtuber_LiveStatus_API_lib as vlsa
-import requests, bs4, re, sys, os, time
+import requests, bs4, re, sys, os
 import django
 sys.path.append("api")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vlive_status_api.settings')
@@ -7,69 +7,17 @@ django.setup()
 from api.models import *
 from tqdm import tqdm
 
+#BASE_URL = 'http://localhost:8000/api/' #local
+BASE_URL = 'https://vtuber-livestatus-api.herokuapp.com/api/' #heroku
 
-'''
-url = "https://virtual-youtuber.userlocal.jp/office/cover" 
-html_data = requests.get(url)
-parsed = bs4.BeautifulSoup(html_data.content, "html.parser")
-
-
-lis = parsed.find_all('span', class_='text-secondary')
-chname = [i.string for i in lis]
-print(chname)
-
-"""
-propagation = parsed.find_all('a', class_='no-propagation')1
-for s in propagation:
-    print(s.attrs['href'])
-"""
-
-source = 'https://www.youtube.com/results?search_query='#'https://virtual-youtuber.userlocal.jp/'
-
-options = Options()
-options.add_argument("--headless")
-driver = webdriver.Chrome(options=options)
-
-result = []
-
-for c in tqdm(chname):
-    driver.get(source + c)
-    time.sleep(3)
-    p = bs4.BeautifulSoup(driver.page_source, "html.parser")
-    try:
-        ch = p.find('a', class_='yt-simple-endpoint style-scope ytd-channel-renderer').attrs['href']
-    except AttributeError:
-        ch = "none"
-
-    ch = ch.replace('/channel/', '')
-    data = {'uid': ch,
-            'liver_name': c,
-            'production': 'ホロライブ',
-            'gender': 'woman'}
-    result.append(data)
-
-print(result)
-'''
-
-'''BASE_URL = 'https://vtuber-livestatus-api.herokuapp.com/api/' #heroku
-all_liver = vlsa.get(BASE_URL)
-on_liver = vlsa.get(BASE_URL + 'onlive/')
-
-uids = [liver['uid'] for liver in all_liver]
-if len(on_liver) != 0:
-    on_livers = [liver['uid']['uid'] for liver in on_liver]
-else:
-    on_livers = []
-
-
+#channel idを受け取って放送中の場合は配信場所のURL、そうでない場合はFalseを返す
 def live_status(channelid):
     youtube_url = "https://www.youtube.com"
     channel_site_url = youtube_url + "/channel/"
     channel_site_url += channelid
-    html_data = requests.get(channel_site_url)
+    hed = {'Accept-Language': 'ja'}
+    html_data = requests.get(channel_site_url, headers=hed)
     parsed = bs4.BeautifulSoup(html_data.content, "html.parser")
-
-    print(parsed)
 
     element_1 = parsed.find_all("li", text=re.compile("ライブ配信中"))
     element_2 = parsed.find_all("li", text=re.compile("人が視聴中"))
@@ -81,13 +29,34 @@ def live_status(channelid):
     else:
         return False
 
-live_status("UC-hM6YJuNYVAmUWxeIr9FeA")
-'''
+def get_live_title(live_url):
+    html_data = requests.get(live_url)
+    parsed = bs4.BeautifulSoup(html_data.content, "html.parser")
+    return parsed.find('span', class_='watch-title metadata-updateable-title').text
 
-youtube_url = "https://www.youtube.com"
-channel_site_url = youtube_url + "/channel/UC-hM6YJuNYVAmUWxeIr9FeA"
-hed = {'Accept-Language': 'ja'}
-html_data = requests.get(channel_site_url, headers=hed)
-parsed = bs4.BeautifulSoup(html_data.content, "html.parser")
 
-print(parsed.find('html').attrs['lang'])
+all_liver = vlsa.get(BASE_URL)
+on_liver = vlsa.get(BASE_URL + 'onlive/')
+
+uids = [liver['uid'] for liver in all_liver]
+if len(on_liver) != 0:
+    on_livers = [liver['uid']['uid'] for liver in on_liver]
+else:
+    on_livers = []
+
+print(on_livers)
+
+for uid in uids:
+    status = live_status(uid)
+    print(status, uid)
+    if status is not False and uid not in on_livers:
+        try:
+            title = get_live_title(status)
+        except AttributeError:
+            title = "取得失敗"
+        data = {'uid': uid, 'live_title': title, 'live_url': status}
+        #on_liveに追加
+        res = vlsa.post(BASE_URL+'onlive', data)
+    elif status is False and uid in on_livers:
+        #on_liveから外す
+        res = vlsa.delete(BASE_URL+'onlive', uid)
