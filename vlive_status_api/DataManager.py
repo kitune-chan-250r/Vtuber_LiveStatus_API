7cc400acffed1ef8236ef1a9e108658e22cf26b4
kitune-chan-250r <41228665+kitune-chan-250r@users.noreply.github.com>
2019-10-11 23:40:39 +0900

    change DataManager
    asyncioを使った非同期I/Oで処理することにより大幅な高速化を実現

--- vlive_status_api/DataManager.py
+++ vlive_status_api/DataManager.py
@@ -1,75 +1,66 @@
+import asyncio
+import aiohttp
+from bs4 import BeautifulSoup
+import re
 import Vtuber_LiveStatus_API_lib as vlsa
-import requests, bs4, re, sys, os, time
-import django
-sys.path.append("api")
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vlive_status_api.settings')
-django.setup()
-from api.models import *
 from tqdm import tqdm
-import threading
 
-#BASE_URL = 'http://localhost:8000/api/' #local
-BASE_URL = 'https://vtuber-livestatus-api.herokuapp.com/api/' #heroku
 
-#channel idを受け取って放送中の場合は配信場所のURL、そうでない場合はFalseを返す
-def live_status(channelid):
-    youtube_url = "https://www.youtube.com"
-    channel_site_url = youtube_url + "/channel/"
-    channel_site_url += channelid
+async def main(uid):
     hed = {'Accept-Language': 'ja'}
-    html_data = requests.get(channel_site_url, headers=hed)
-    parsed = bs4.BeautifulSoup(html_data.content, "html.parser")
+    url = "https://www.youtube.com/channel/" + uid
+    async with aiohttp.ClientSession(headers=hed) as session:
+        async with session.get(url) as response:
+            html = await response.text()
+            parsed = BeautifulSoup(html, "html.parser")
+            
+            element_1 = parsed.find_all("li", text=re.compile("ライブ配信中"))
+            element_2 = parsed.find_all("li", text=re.compile("人が視聴中"))
 
-    element_1 = parsed.find_all("li", text=re.compile("ライブ配信中"))
-    element_2 = parsed.find_all("li", text=re.compile("人が視聴中"))
+            if len(element_1) > 0 and len(element_2) > 0:
+                watch = parsed.find("a", class_="yt-uix-sessionlink yt-uix-tile-link spf-link yt-ui-ellipsis yt-ui-ellipsis-2").attrs['href']
+                title = parsed.find("a", class_="yt-uix-sessionlink yt-uix-tile-link spf-link yt-ui-ellipsis yt-ui-ellipsis-2").text
+                result = {'watch': watch.replace('/watch?v=', ''), 'title': title, 'uid': uid, 'status': True}
+            else:
+                result = {'uid': uid, 'status': False}
+    return result
 
-    if len(element_1) > 0 and len(element_2) > 0:
-        watch = parsed.find("a", class_="yt-uix-sessionlink yt-uix-tile-link spf-link yt-ui-ellipsis yt-ui-ellipsis-2").attrs['href']
-        title = parsed.find("a", class_="yt-uix-sessionlink yt-uix-tile-link spf-link yt-ui-ellipsis yt-ui-ellipsis-2").text
-        return {'watch': watch.replace('/watch?v=', ''), 'title': title}
-    else:
-        return False
+BASE_URL = 'https://vtuber-livestatus-api.herokuapp.com/api/' 
 
 all_liver = vlsa.get(BASE_URL)
 on_liver = vlsa.get(BASE_URL + 'onlive/')
-
-uids = [liver['uid'] for liver in all_liver]
 if len(on_liver) != 0:
     on_livers = [liver['uid']['uid'] for liver in on_liver]
 else:
     on_livers = []
 
-div = len(all_liver) // 2
+uids = [liver['uid'] for liver in all_liver]
 
-ll1 = uids[:div]
-ll2 = uids[div:]
+loop = asyncio.get_event_loop()
+done,pending = loop.run_until_complete(
+    asyncio.wait([main(uid) for uid in tqdm(uids)]))
 
-def reqtask(uids):
-    for uid in uids:
-        status = live_status(uid)
-        print(status, uid)
-        if status is not False and uid not in on_livers:
-            data = {'uid': uid, 'live_title': status['title'],
-                 'live_url': 'https://www.youtube.com/watch?v='+status['watch']}
-            #on_liveに追加
+res = [d.result() for d in done] #結果
+len(res)
+for r in tqdm(res):
+    #1つ前の更新で放送中ではなかったが返ってきたステータスが放送中だった場合
+    if r['status'] is not False and r['uid'] not in on_livers:
+        data = {'uid': r['uid'], 'live_title': r['title'],
+                'live_url': 'https://www.youtube.com/watch?v='+r['watch']}
+        #on_liveに追加
+        print(data)
+        res = vlsa.post(BASE_URL+'onlive', data)
+    
+    #1つ前の更新で放送中で返ってきたステータスも放送中だがタイトルが変わっていた場合
+    #短期間に2度続けて放送するライバーに対応するための処理
+    elif r['status'] is not False and r['uid'] in on_livers:
+        title = [l['live_title'] for l in on_liver if l['uid']['uid'] == r['uid']]
+        if r['title'] != title[0]:
+            res = vlsa.delete(BASE_URL+'onlive', r['uid'])
+            data = {'uid': r['uid'], 'live_title': r['title'],
+                    'live_url': 'https://www.youtube.com/watch?v='+r['watch']}
             res = vlsa.post(BASE_URL+'onlive', data)
-
-        elif status is not False and uid in on_livers:
-            title = [l['live_title'] for l in on_liver if l['uid']['uid'] == uid]
-            print(title, status['title'])
-            if status['title'] != title[0]:
-                res = vlsa.delete(BASE_URL+'onlive', uid)
-                data = {'uid': uid, 'live_title': status['title'],
-                 'live_url': 'https://www.youtube.com/watch?v='+status['watch']}
-                res = vlsa.post(BASE_URL+'onlive', data)
-
-        elif status is False and uid in on_livers:
-            #on_liveから外す
-            res = vlsa.delete(BASE_URL+'onlive', uid)
-
-
-th1 = threading.Thread(target=reqtask, args=(ll1,))
-th2 = threading.Thread(target=reqtask, args=(ll2,))
-
-th1.start()
-th2.start()
+    
+    #1つ前の更新で放送中で返ってきたステータスが放送中ではなかった場合
+    elif r['status'] is False and r['uid'] in on_livers:
+        res = vlsa.delete(BASE_URL+'onlive', r['uid'])
