8f0d8d8277a6bf3fa85d385cf10473c1c37fa6e6
kitune-chan-250r <41228665+kitune-chan-250r@users.noreply.github.com>
2020-07-15 01:57:05 +0900

    new datamanager test

--- debug.py
+++ debug.py
@@ -2,33 +2,48 @@ import asyncio
 import aiohttp
 from bs4 import BeautifulSoup
 import re
-from tqdm import tqdm
-import requests
-import requests_html as rh
-import Vtuber_LiveStatus_API_lib as vlsa
-
-BASE_URL = 'https://vtuber-livestatus-api.herokuapp.com/api/'
 
 async def main(uid):
     hed = {'Accept-Language': 'ja'}
     url = "https://www.youtube.com/channel/" + uid
-    asysession = rh.AsyncHTMLSession()
+    async with aiohttp.ClientSession(headers=hed) as session:
+        async with session.get(url) as response:
+            html = await response.text()
+            parsed = BeautifulSoup(html, "html.parser")
+            
+            element_1 = parsed.find_all('script', text=re.compile("ライブ配信中"))
+            element_2 = parsed.find_all('script', text=re.compile("人が視聴中"))
 
-    asysession.headers.update(hed)
-    r = await asysession.get(url)
-    await r.html.arender()
+            if len(element_1) > 0 and len(element_2) > 0:
+                for scrp in parsed.find_all("script"):
+                    if "window[\"ytInitialData\"]" in scrp.text:
+                        dict_str = scrp.text.split(" = ")[1]
 
-    element = r.html.find("span.ytd-shelf-renderer")[0].text
-    if element == 'ライブ配信中':
-        livedetail = r.html.find('a.yt-simple-endpoint.style-scope.ytd-video-renderer')[0].attrs
-        result = {'watch': livedetail['href'].replace('/watch?v=', ''), 'title': livedetail['title'], 'uid': uid, 'status': True}
-    else:
-        result = {'uid': uid, 'status': False}
+                        dict_str = dict_str.replace("false","False")
+                        dict_str = dict_str.replace("true","True")
 
+                        dict_str = dict_str.rstrip("  \n;")
 
+                        dict_str = dict_str.replace('    window["ytInitialPlayerResponse"]', '')
+                        dict_str = dict_str.replace(";", "")
+                        
+                        dics = eval(dict_str)
+                        break
+
+                stream_description = dics["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][0]\
+                                        ["tabRenderer"]["content"]["sectionListRenderer"]["contents"][1]\
+                                        ['itemSectionRenderer']['contents'][0]\
+                                        ['shelfRenderer']['content']['expandedShelfContentsRenderer']['items'][0]\
+                                        ['videoRenderer']
+
+                watch = stream_description['videoId']
+                title = stream_description['title']['simpleText']
+                result = {'watch': watch, 'title': title, 'uid': uid, 'status': True}
+            else:
+                result = {'uid': uid, 'status': False}
     return result
 
-#uids = ["UCSFCh5NL4qXrAy9u-u2lX3g", "UCqm3BQLlJfvkTsX_hvm0UmA"]
+BASE_URL = 'https://vtuber-livestatus-api.herokuapp.com/api/' 
 
 all_liver = vlsa.get(BASE_URL)
 on_liver = vlsa.get(BASE_URL + 'onlive/')
@@ -39,12 +54,12 @@ else:
 
 uids = [liver['uid'] for liver in all_liver]
 
+
 loop = asyncio.get_event_loop()
 done,pending = loop.run_until_complete(
-    asyncio.wait([main(uid) for uid in tqdm(uids)]))
+    asyncio.wait([main(uid) for uid in uids]))
 
-res = [d.result() for d in done]
-
+res = [d.result() for d in done] #結果
 len(res)
 for r in tqdm(res):
     #1つ前の更新で放送中ではなかったが返ってきたステータスが放送中だった場合
@@ -54,7 +69,7 @@ for r in tqdm(res):
         #on_liveに追加
         print(data)
         res = vlsa.post(BASE_URL+'onlive', data)
-
+    
     #1つ前の更新で放送中で返ってきたステータスも放送中だがタイトルが変わっていた場合
     #短期間に2度続けて放送するライバーに対応するための処理
     elif r['status'] is not False and r['uid'] in on_livers:
@@ -64,15 +79,7 @@ for r in tqdm(res):
             data = {'uid': r['uid'], 'live_title': r['title'],
                     'live_url': 'https://www.youtube.com/watch?v='+r['watch']}
             res = vlsa.post(BASE_URL+'onlive', data)
-
+    
     #1つ前の更新で放送中で返ってきたステータスが放送中ではなかった場合
     elif r['status'] is False and r['uid'] in on_livers:
         res = vlsa.delete(BASE_URL+'onlive', r['uid'])
-
-
-'''session = rh.HTMLSession()
-r = session.get('https://www.youtube.com/channel/UCSFCh5NL4qXrAy9u-u2lX3g')
-r.html.render()
-element = r.html.find('a.yt-simple-endpoint.style-scope.ytd-video-renderer')[0].attrs
-
-print(element)'''
--- test.py
+++ test.py
@@ -39,3 +39,114 @@ def live_status(channelid):
 
 
 live_status(uid)
+
+import asyncio
+import aiohttp
+import re
+from tqdm import tqdm
+import requests
+import random
+from bs4 import BeautifulSoup
+import json
+
+
+"""api_keys = ['AIzaSyAf6M7VIuXyfkZVnII_WNIXunbiN2Po0qU',
+            'AIzaSyDc4eiGx2LJUFgdGI453NXnNpJwEzfbHHI',
+            'AIzaSyBd_ljjcFJo5rC6IIh8IfztcCHc5OtTR24',
+            'AIzaSyCB4efFgu8H0sIFl9u5CQttYh1Wdit0Ueg',
+            'AIzaSyA0-ZTkYVrSwME51qtCNuv2lvsllw8fXX8',
+            'AIzaSyBV8dDWpE41q32JYANzBwfoXtL-ZxTOiMU',
+            'AIzaSyCs7S3OBW7F2U4v8Y5tTSuqzDJbI-8uVXo',
+            'AIzaSyAP5ZIgh6gwjEKNndaZKRFRBkei2LrhJBo',
+            'AIzaSyDCtEe4Cv_DmXPCN8oBa7v-zTd_xh7PPqg',
+            'AIzaSyA-FV3AZuBBxXvCUNeUnWL1_sB5bcUpWIg',
+            'AIzaSyCjtiTj08c2t86HB5T5XrGTFnzqOEjAFXQ',
+            'AIzaSyCOXBSQxEKPCl0tyeOI4SqjaAirs2qo0Ew',
+            'AIzaSyBp6hZ0qm99sxCPUPe6ir_8sjkBmueFEMM',
+            'AIzaSyASyQPi9A5r6zJco4GS87gOcY4u80sCC1Q',
+            'AIzaSyCsBEUIRLaRF7O6nRl7YC9zpzdAwUNaFk4',
+            'AIzaSyDOdfML2WLRSZSq5ATKq4mH98MlqAf7cBk',
+            'AIzaSyCfhWXZnPBrjQth3eI4FcnYn9p_rJyr-2E',
+            'AIzaSyAJ8R_BfgNWQjiaFsgqRDi7OpKZRLxth1Q',
+            'AIzaSyCJvydjvbUckYSP_vnMvlq-6mID29rs_Ms',
+            'AIzaSyAroxsOf5nKtIL86G9pCU9zXtsOMkNU1p8',
+            'AIzaSyCt50hyGXY6k3RDe195yM86ZLn4DV8Lmxg',
+            'AIzaSyC4G1w1DAKAv2G6S7Lo8JECjM8Xka8H8TQ',
+            'AIzaSyDCHtArBNvpKNAeqo4j8xsE_omXRHdJKN0',
+            'AIzaSyBQ8SLkyNG7vwI9Ym6YRVcOjT8kimxyyXs',
+            'AIzaSyC9PSHYKIyhwEJfszxxNYluvFSaLV0mJs4']
+
+'''async def main(uid):
+    hed = {'Accept-Language': 'ja'}
+    url = "https://www.youtube.com/channel/" + uid
+    async with aiohttp.ClientSession(headers=hed) as session:
+        async with session.get(url) as response:
+            html = await response.text()
+           
+    return result'''
+BASE_URL = 'https://vtuber-livestatus-api.herokuapp.com/api/' 
+api_main = 'https://www.googleapis.com/youtube/v3/'
+search_channel = 'search?part=snippet&channelId='
+channelID = 'UCQ0UDLQCjY0rmuxCDE38FGg'
+#r = requests.get(api_main + search_channel + channelID + '&key=' + api_keys[0] + '&type=video&eventType=live')
+
+all_liver = requests.get(BASE_URL).json()
+uids = [liver['uid'] for liver in all_liver]
+
+for uid in uids:
+    keynumber = random.randint(0, 24)
+    print(keynumber)
+    key = api_keys[keynumber]
+    print(key)
+    r = requests.get(api_main + search_channel + uid + '&key=' + key + '&type=video&eventType=live').json()
+    #r['']
+    if r['items'] != []:
+        result = {'watch': r['items'][0]['id']['videoId'], 'title': r['items'][0]['snippet']['title'], 'uid': uid, 'status': True}
+    else:
+        result = {'uid': uid, 'status': False}
+
+    print(result)
+
+
+"""
+
+url = 'https://www.youtube.com/channel/UCspv01oxUFf_MTSipURRhkA'
+res = requests.get(url).text
+parsed = BeautifulSoup(res, "html.parser") #配信中２７
+
+import dukpy
+
+script1 = parsed.find_all('script', text=re.compile("ライブ配信中"))
+script2 = parsed.find_all('script', text=re.compile("人が視聴中"))
+
+#print(len(script2))
+
+#
+
+
+for scrp in parsed.find_all("script"):
+    if "window[\"ytInitialData\"]" in scrp.text:
+        dict_str = scrp.text.split(" = ")[1]
+
+        # javascript表記なので更に整形. falseとtrueの表記を直す
+        dict_str = dict_str.replace("false","False")
+        dict_str = dict_str.replace("true","True")
+
+        # 辞書形式と認識すると簡単にデータを取得できるが, 末尾に邪魔なのがあるので消しておく（「空白2つ + \n + ;」を消す）
+        dict_str = dict_str.rstrip("  \n;")
+
+        dict_str = dict_str.replace('    window["ytInitialPlayerResponse"]', '')
+        dict_str = dict_str.replace(";", "")
+        # 辞書形式に変換
+        dics = eval(dict_str)
+        break
+
+stream_description = dics["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][0]\
+                        ["tabRenderer"]["content"]["sectionListRenderer"]["contents"][1]\
+                        ['itemSectionRenderer']['contents'][0]\
+                        ['shelfRenderer']['content']['expandedShelfContentsRenderer']['items'][0]\
+                        ['videoRenderer']
+
+watch = stream_description['videoId']
+title = stream_description['title']['simpleText']
+print(title)
