4f47bf0b735b6f9a3d79e16ca31f6dd2bb9204a2
kitune-chan-250r <41228665+kitune-chan-250r@users.noreply.github.com>
2020-09-22 21:09:35 +0900

    undo

--- api/admin.py
+++ api/admin.py
@@ -3,11 +3,11 @@ from .models import Vtuber, On_Live
 
 # Register your models here.
 class VtuberFrom(admin.ModelAdmin):
-	fields = ['uid', 'liver_name', 'production', 'gender', 'src']
+	fields = ['uid', 'liver_name', 'production', 'gender']#, 'src'
 
 admin.site.register(Vtuber, VtuberFrom)
 
 class On_liveFrom(admin.ModelAdmin):
-	fields = ['uid', 'live_title']
+	fields = ['uid', 'live_title', 'viewer']
 
 admin.site.register(On_Live, On_liveFrom)
--- api/serializers.py
+++ api/serializers.py
@@ -4,18 +4,18 @@ from .models import Vtuber, On_Live
 class VtuberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vtuber
-        fields = ('uid', 'liver_name', 'production', 'gender')#'src'
+        fields = ('uid', 'liver_name', 'production', 'gender')#, 'src'
 
 
 class OnLiveSerializer(serializers.ModelSerializer):
     uid = VtuberSerializer()
     class Meta:
         model = On_Live
-        fields = ('uid', 'start_time', 'live_title', 'live_url')
+        fields = ('uid', 'start_time', 'live_title', 'live_url', 'viewer')
 
 #uidのみでPOSTするためのシリアライザ
 class OnLive_POST_Serializer(serializers.ModelSerializer):
     uid = serializers.PrimaryKeyRelatedField(queryset=Vtuber.objects.all())
     class Meta:
         model = On_Live
-        fields = ('uid', 'start_time', 'live_title', 'live_url')
+        fields = ('uid', 'live_title', 'live_url', 'viewer')#, 'start_time'
