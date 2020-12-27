6d90946d367967a4c1f0c219df772e29a39b4c89
kitune-chan-250r <41228665+kitune-chan-250r@users.noreply.github.com>
2020-12-27 16:38:12 +0900

    fix bug

--- DataManager.py
+++ DataManager.py
@@ -77,6 +77,8 @@ async def main(uid):
             element_2 = parsed.find_all('script', text=re.compile("人が視聴中"))
             remind = parsed.find_all('script', text=re.compile("今後のライブ ストリーム"))
             
+            result = {}
+
             if len(element_2) or len(remind) > 0:
                 for scrp in parsed.find_all("script"):
                     if "var ytInitialData" in scrp.text:
@@ -106,11 +108,10 @@ async def main(uid):
                                             ['itemSectionRenderer']['contents'][0]\
                                             ['channelFeaturedContentRenderer']['items'][0]['videoRenderer']
                 except KeyError:
-                    result = {'uid': uid, 'status': False}
+                    result['onlive'] = {'uid': uid, 'status': False}
 
                 else:
                     watch = stream_description['videoId']
-                    
                     
                     try:
                         #title = stream_description['title']['simpleText']
@@ -118,7 +119,7 @@ async def main(uid):
                     except KeyError:
                         title = "データ取得失敗 KeyError: stream_description['title']['simpleText']"
 
-                    result = {'watch': watch, 'title': title, 'uid': uid, 'status': True, 'flag': 'onlive'}
+                    result['onlive'] = {'watch': watch, 'title': title, 'uid': uid, 'status': True, 'flag': 'onlive'}
 
             elif len(remind) > 0:
                 reminder_description = dics["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][0]\
@@ -149,10 +150,11 @@ async def main(uid):
                                 audience = reminds['shortViewCountText']['runs'][0]['text']
                             except KeyError:
                                 audience = 0
-                            result = {'watch': reminder_watch, 'title': reminder_title, 'uid': uid,'start_datetime': reminder_date, 'audience': audience, 'flag': 'reminder'}
+                            result['reminder'] = {'watch': reminder_watch, 'title': reminder_title, 'uid': uid,'start_datetime': reminder_date, 'audience': audience, 'flag': 'reminder'}
+
                             break
             else:
-                result = {'uid': uid, 'status': False, 'flag': 'onlive'}
+                result['onlive'] = {'uid': uid, 'status': False, 'flag': 'onlive'}
     return result
 
 BASE_URL = 'https://vtuber-livestatus-api.herokuapp.com/api/' 
@@ -181,7 +183,8 @@ res = [d.result() for d in done] #結果
 len(res)
 for r in res:
     #1つ前の更新で放送中ではなかったが返ってきたステータスが放送中だった場合
-    if r['flag'] == 'onlive' and r['status'] is not False and r['uid'] not in on_livers:
+    if r['onlive']['status'] is not False and r['onlive']['uid'] not in on_livers:
+        r = r['onlive']
         data = {'uid': r['uid'], 'live_title': r['title'],
                 'live_url': 'https://www.youtube.com/watch?v='+r['watch']}
         #on_liveに追加
@@ -190,7 +193,8 @@ for r in res:
     
     #1つ前の更新で放送中で返ってきたステータスも放送中だがタイトルが変わっていた場合
     #短期間に2度続けて放送するライバーに対応するための処理
-    elif r['flag'] == 'onlive' and r['status'] is not False and r['uid'] in on_livers:
+    elif r['onlive']['status'] is not False and r['onlive']['uid'] in on_livers:
+        r = r['onlive']
         title = [l['live_title'] for l in on_liver if l['uid']['uid'] == r['uid']]
         if r['title'] != title[0]:
             res = vlsa.delete(BASE_URL+'onlive', r['uid'])
@@ -199,11 +203,13 @@ for r in res:
             res = vlsa.post(BASE_URL+'onlive', data)
     
     #1つ前の更新で放送中で返ってきたステータスが放送中ではなかった場合
-    elif r['flag'] == 'onlive' and r['status'] is False and r['uid'] in on_livers:
+    elif r['onlive']['status'] is False and r['onlive']['uid'] in on_livers:
+        r = r['onlive']
         res = vlsa.delete(BASE_URL+'onlive', r['uid'])
 
     #ここからreminder
-    elif r['flag'] == 'reminder':
+    elif 'reminder' in r:
+        r = r['reminder']
         date = datetime.datetime.fromtimestamp(int(r['start_datetime']), datetime.timezone(datetime.timedelta(hours=9)))
         now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
         gap = date - now
