0f6f1811dd77abcc8f564b8fd9f4825837009b50
kitune-chan-250r <41228665+kitune-chan-250r@users.noreply.github.com>
2020-12-27 15:08:27 +0900

    Update DataManager.py

--- DataManager.py
+++ DataManager.py
@@ -207,7 +207,7 @@ for r in res:
         date = datetime.datetime.fromtimestamp(int(r['start_datetime']), datetime.timezone(datetime.timedelta(hours=9)))
         now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
         gap = date - now
-        if gap < 1:
+        if int(gap) < 1:
             data = {'uid': r['uid'], 'start_datetime': r['start_datetime'], 'live_title': r['title'],
                     'live_url': 'https://www.youtube.com/watch?v='+r['watch'], 'audience': r['audience']} #'uid', 'start_datetime', 'live_title', 'live_url', 'audience'
             res = vlsa.post(BASE_URL+'reminder/', data)
